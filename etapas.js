8aade9d6b2df1299a45901fbf4a9fd919090ac27
Raphael Melo <raphaanjos1@gmail.com>
2022-01-12 17:45:35 -0300

    implementado o arquivo que contém as informações dos candidados a eleição.

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: 'candidate_3.jpg', legenda: 'Vereadora', samll: false }
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano de Tal',
+                partido: 'DEFG',
+                fotos: [
+                    { url: 'candidate_4.jpg', legenda: 'Vereador', samll: false }
+                ]
+            },
+
+        ],
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 85,
+                nome: 'Fulano',
+                partido: 'OXI',
+                vice: 'Fulaninho',
+                fotos: [
+                    { url: 'candidate_5.jpg', legenda: 'Prefeito' },
+                    { url: 'candidate_6.jpg', legenda: 'Vice-Prefeito', samll: true }
+                ]
+            },
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'OXENTE',
+                vice: 'Zulaninho',
+                fotos: [
+                    { url: 'candidate_7.jpg', legenda: 'Prefeito' },
+                    { url: 'candidate_8.jpg', legenda: 'Vice-Prefeito', samll: true }
+                ]
+            },
+
+        ]
+    }
+]
